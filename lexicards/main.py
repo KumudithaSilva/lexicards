700ca7114f3da611e68f0a974bfd3819394ef80c
Kumuditha <kumudithasilva66@gmail.com>
2025-10-09 19:58:31 +0530

    add images and initialize lexiui

--- lexicards/main.py
+++ lexicards/main.py
@@ -1,6 +1,7 @@
 from tkinter import Tk
 
 from lexicards.resources import ResourceLoader
+from lexicards.ui import LexiUI
 
 
 def main():
@@ -8,10 +9,12 @@ def main():
     loader = ResourceLoader()
 
     images = {
-        "main_logo": loader.load_image("lexicards.png"),
-        "main_icon": loader.get_resource_path("lexicards.ico"),
+        "card_front_image": loader.load_image("card_front.png"),
+        "wrong_image": loader.load_image("wrong.png"),
+        "right_image": loader.load_image("right.png"),
     }
 
+    ui = LexiUI(root, images)
     root.mainloop()
 
 
