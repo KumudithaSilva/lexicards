cb8254a73b11cf4c459b28ce9c4c5138f7452843
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-10-15 23:32:03 +0530

    Add LexiCardError

---
+++ lexicards/errors.py
@@ -0,0 +1,22 @@
+class LexiCardError(Exception):
+    """
+    Base class for all LexiCard-specific exceptions.
+    """
+
+    pass
+
+
+class DataFileNotFoundError(LexiCardError):
+    """
+    Raised when the required data file cannot be found.
+    """
+
+    pass
+
+
+class DataCorruptionError(LexiCardError):
+    """
+    Raised when a data file is found but is corrupted or unreadable.
+    """
+
+    pass
