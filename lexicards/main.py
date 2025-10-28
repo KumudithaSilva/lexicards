471787ecf3034d0d540039bf2fc9e6be8196bb22
Kumuditha <kumudithasilva66@gmail.com>
2025-10-28 19:50:04 +0530

    Add images with loader, ui, retriever and controller

--- lexicards/main.py
+++ lexicards/main.py
@@ -1,12 +1,24 @@
 from tkinter import Tk
+
+from lexicards.controllers.data_loader import ResourceLoader
 from lexicards.controllers.data_retriever import CSVDataRetrieverFactory
+from lexicards.controllers.lexi_ui import LexiUi
+from lexicards.controllers.lexical_controller import LexicalController
 
 
 def main():
     root = Tk()
+    loader = ResourceLoader()
 
-    factory = CSVDataRetrieverFactory("data/japanese_words.csv")
-    factory.create_data_retriever()
+    images = {
+        "card_front_image": loader.load_image("card_front.png"),
+        "wrong_image": loader.load_image("wrong.png"),
+        "right_image": loader.load_image("right.png"),
+    }
+
+    ui = LexiUi(root, images)
+    retriever = CSVDataRetrieverFactory("data/japanese_words.csv").create_data_retriever()
+    LexicalController(ui, retriever)
 
     root.mainloop()
 
