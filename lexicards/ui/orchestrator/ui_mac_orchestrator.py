c0b0f373da09bf750a13aa2b482cbaef1c0a1354
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-11-05 00:48:34 +0530

    Added Mac-specific UiOrchestrator

---
+++ lexicards/ui/orchestrator/ui_mac_orchestrator.py
@@ -0,0 +1,9 @@
+from lexicards.ui.orchestrator.ui_orchestrator import UiOrchestrator
+
+
+class MacUiOrchestrator(UiOrchestrator):
+    """Adds Mac-specific audio wiring."""
+
+    def wire_callbacks(self, controller):
+        super().wire_callbacks(controller)
+        self.ui.set_audio_callback(controller.generate_random_unknown_word)
