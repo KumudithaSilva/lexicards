4e7d6372828880affcf2d9b6495e78bbc7052bda
Kumuditha <kumudithasilva66@gmail.com>
2025-11-05 21:56:47 +0530

    added concrete scheduler

--- lexicards/ui/orchestrator/ui_orchestrator.py
+++ lexicards/ui/orchestrator/ui_orchestrator.py
@@ -11,3 +11,12 @@ class UiOrchestrator(IUiOrchestrator):
         """Wire UI button events to controller logic."""
         self.ui.set_known_callback(controller.generate_random_new_word)
         self.ui.set_unknown_callback(controller.generate_random_unknown_word)
+        self.schedule_auto_meaning(controller)
+
+    def schedule_auto_meaning(self, controller):
+        """Automatically refresh meaning every few seconds."""
+        def update_meaning():
+            controller.generate_word_meaning()
+            self.ui.get_root().after(5000, update_meaning)
+        self.ui.get_root().after(5000, update_meaning)
+
