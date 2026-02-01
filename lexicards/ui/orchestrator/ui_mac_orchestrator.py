51dcf16a9827643f93104b014426de41b487500e
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-02-01 23:39:40 +0530

    add controller and MacUiOrchestrator handel speak

--- lexicards/ui/orchestrator/ui_mac_orchestrator.py
+++ lexicards/ui/orchestrator/ui_mac_orchestrator.py
@@ -6,4 +6,4 @@ class MacUiOrchestrator(UiOrchestrator):
 
     def wire_callbacks(self, controller):
         super().wire_callbacks(controller)
-        self.ui.set_audio_callback(controller.handle_next_word)
+        self.ui.set_audio_callback(controller.handle_speak)
